5e4b2e87db2c8e2cc0f54eab4200c3a99fe7cb7e
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-20 01:54:36 +0800

    Backend Temp

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3306
+const multer = require('multer');
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
@@ -15,6 +17,17 @@ app.get('/', (req, res) => {
 
 const userRoutes = require('./src/routes/users.route');
 app.use('/api/users', userRoutes);
+
+const userImageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../frontend/src/assets/userimage') 
+    }
+});
+
+const upload = multer({ storage: userImageStorage });
+app.post('/upload-user-image', upload.single('image'), function (req, res, next) {
+    res.status(200).json({ message: 'User image uploaded successfully' });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
