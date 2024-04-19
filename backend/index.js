777dea250379622440f682e6c98585cb3d820b2c
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-20 03:20:19 +0800

    Backend Temp

--- backend/index.js
+++ backend/index.js
@@ -17,16 +17,15 @@ const userRoutes = require('./src/routes/users.route');
 app.use('/api/users', userRoutes);
 
 const multer = require('multer');
-const path = require('path'); // Add this line to import the 'path' module
 
 // Define storage for uploaded files
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, '../frontend/src/assets/userimage'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, '/opt/render/project/src/frontend/src/assets/userimage/'); // Set the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Maintain the original filename
+  }
 });
 
 // Initialize multer instance with the storage options
@@ -34,12 +33,11 @@ const upload = multer({ storage: storage });
 
 // Endpoint to handle file upload
 app.post('/api/upload-user-image', upload.single('profileImage'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-    }
-    res.status(200).json({ message: 'File uploaded successfully' });
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+  res.status(200).json({ message: 'File uploaded successfully' });
 });
-
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
