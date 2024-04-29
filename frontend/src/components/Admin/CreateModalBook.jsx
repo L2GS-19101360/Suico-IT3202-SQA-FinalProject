6cbfd1d7664f97ce391a8c3012d08ab3560c1408
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-30 02:23:35 +0800

    Frontend Updated

---
+++ frontend/src/components/Admin/CreateModalBook.jsx
@@ -0,0 +1,230 @@
+import { Component, useState } from 'react'
+import { Container, Nav, Navbar, NavDropdown, Button, Form, InputGroup, Dropdown, Table, Modal } from 'react-bootstrap'
+import webName from '../../assets/website name.jpg'
+import ClockComponent from '../../components/ClockComponent'
+import AdminSidebar from '../../components/Admin/AdminSidebar'
+import AdminNavbar from '../../components/Admin/AdminNavbar'
+import axios from 'axios'
+import { FaSearch, FaLock, FaUnlockAlt, FaBook } from 'react-icons/fa'
+
+class CreateModalBook extends Component {
+
+    constructor() {
+        super();
+        this.handleShow = this.handleShow.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+        this.handleImageChange = this.handleImageChange.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+        this.storeBook = this.storeBook.bind(this);
+        this.state = {
+            show: false,
+
+            bookImageUrl: null,
+            bookImageFile: null,
+
+            bookContentUrl: null,
+            bookContentFile: null,
+
+            bookImageURL: "",
+            bookTitle: "",
+            authorName: "",
+            selectedGenre: "Select Genre",
+            bookGenre: "",
+            bookContentURL: ""
+        }
+    }
+
+    componentDidMount() {
+
+    }
+    componentWillUnmount() {
+
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    handleImageChange(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const fileName = file.name;
+            this.setState({
+                bookImageUrl: URL.createObjectURL(file) || null, // Reset to null when new image selected
+                bookImageFile: file
+            });
+        }
+    }
+    // Update handleContentChange method to handle PDF files
+    handleContentChange(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const fileName = file.name;
+            // Ensure the file type is PDF before setting the state
+            if (file.type === 'application/pdf') {
+                this.setState({
+                    bookContentUrl: URL.createObjectURL(file),
+                    bookContentFile: file
+                });
+            } else {
+                alert('Please select a PDF file.');
+            }
+        }
+    }
+
+    storeBook = async (event) => {
+        event.preventDefault();
+
+        const selectedImageFile = this.state.bookImageFile;
+        const selectedImageContent = this.state.bookContentFile
+        const apiLinks = [
+            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/book-image',
+            'http://localhost:3306/api/book-image',
+            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/book-content',
+            'http://localhost:3306/api/book-content',
+            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/books/',
+            'http://localhost:3306/api/books/'
+        ];
+
+        if (selectedImageFile) {
+            const formImageData = new FormData();
+            formImageData.append('file', selectedImageFile);
+            try {
+                const response = await axios.post(apiLinks[0], formImageData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                });
+                console.log('File uploaded successfully:', response.data);
+                console.log('Public URL:', response.data.imageUrl);
+
+                this.setState({ bookImageURL: response.data.imageUrl }, async () => {
+                    if (selectedImageContent) {
+                        const formContentData = new FormData();
+                        formContentData.append('file', selectedImageContent);
+
+                        try {
+                            const response = await axios.post(apiLinks[2], formContentData, {
+                                headers: {
+                                    'Content-Type': 'multipart/form-data'
+                                },
+                            });
+                            console.log('File uploaded successfully:', response.data);
+                            console.log('Public URL:', response.data.pdfUrl);
+
+                            this.setState({ bookContentURL: response.data.pdfUrl }, () => {
+                                console.log('Book Image URL:', this.state.bookImageURL);
+                                console.log('Book Title:', this.state.bookTitle);
+                                console.log('Author Name:', this.state.authorName);
+                                console.log('Selected Genre:', this.state.selectedGenre);
+                                console.log('Book Content URL:', this.state.bookContentURL);
+
+                                const data = {
+                                    image: this.state.bookImageURL,
+                                    name: this.state.bookTitle,
+                                    author: this.state.authorName,
+                                    genre: this.state.selectedGenre,
+                                    content: this.state.bookContentURL
+                                }
+
+                                axios.post(
+                                    apiLinks[4], data
+                                ).then(
+                                    (response) => {
+                                        console.log(response);
+
+                                        window.location.reload();
+                                    }
+                                ).catch(
+                                    (error) => {
+                                        console.log(error);
+                                    }
+                                );
+                            });
+                        } catch (error) {
+                            console.error('Error uploading file:', error);
+                        }
+                    }
+                });
+
+            } catch (error) {
+                console.error('Error uploading file:', error);
+            }
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Button variant="success" onClick={this.handleShow}>
+                    <FaBook /> Create New Book
+                </Button>
+
+                <Modal show={this.state.show} onHide={this.handleClose} size='lg' aria-labelledby="contained-modal-title-vcenter" centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title><FaBook /> Create New Book</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <div style={{ display: 'inline-flex' }}>
+                                <div id='leftDiv'>
+                                    {this.state.bookImageUrl ? (
+                                        <img
+                                            src={this.state.bookImageUrl}
+                                            alt="Book Cover"
+                                            height={300}
+                                            width={200}
+                                        />
+                                    ) : (
+                                        <img src="" alt="" height={300} width={200} />
+                                    )}
+                                </div>
+                                <div id='rightDiv' style={{ flex: '1', marginLeft: '20px', width: '469px' }}>
+                                    <Form.Label>Book Image</Form.Label>
+                                    <Form.Group controlId="formFile" className="mb-3">
+                                        <Form.Control type="file" accept="image/jpeg, image/png" onChange={this.handleImageChange} />
+                                    </Form.Group>
+                                    <Form.Control type="text" placeholder="Enter Book Title" value={this.state.bookTitle} onChange={(e) => (this.setState({ bookTitle: e.target.value }))} /><br />
+                                    <Form.Control type="text" placeholder="Enter Book Author" value={this.state.authorName} onChange={(e) => (this.setState({ authorName: e.target.value }))} /><br />
+
+                                    <Dropdown>
+                                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                            {this.state.selectedGenre}
+                                        </Dropdown.Toggle>
+                                        
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Historical Fiction" })}>Historical Fiction</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Crime and Mystery" })}>Crime and Mystery</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Horror" })}>Horror</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Science Fiction" })}>Science Fiction</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Fantasy" })}>Fantasy</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Education" })}>Education</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => this.setState({ selectedGenre: "Romance" })}>Romance</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown><br />
+
+                                    <Form.Label>Book Content</Form.Label>
+                                    <Form.Group controlId="formFile" className="mb-3">
+                                        <Form.Control type="file" accept=".pdf" onChange={this.handleContentChange} />
+                                    </Form.Group>
+                                    <Button variant="success" onClick={this.storeBook} type='submit' style={{ marginTop: "10px" }}>Store Book</Button>
+                                </div>
+                            </div>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+
+}
+
+export default CreateModalBook
--- frontend/src/pages/Admin/ManageBooks.jsx
+++ frontend/src/pages/Admin/ManageBooks.jsx
@@ -1,121 +1,53 @@
 import { Component, useState } from 'react'
-import { Container, Nav, Navbar, NavDropdown, Button, Form, InputGroup, Dropdown, Table, Modal } from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown, Button, Form, InputGroup, Dropdown, Table, Modal, Spinner } from 'react-bootstrap'
 import webName from '../../assets/website name.jpg'
 import ClockComponent from '../../components/ClockComponent'
 import AdminSidebar from '../../components/Admin/AdminSidebar'
 import AdminNavbar from '../../components/Admin/AdminNavbar'
 import { withRouter } from 'react-router-dom'
-import { FaSearch, FaLock, FaUnlockAlt, FaBook } from 'react-icons/fa'
+import { FaSearch, FaLock, FaUnlockAlt, FaBook, FaSync, FaTrash, FaBookOpen } from 'react-icons/fa'
 import axios from 'axios'
+import CreateModalBook from '../../components/Admin/CreateModalBook'
+import { NavLink } from 'react-router-dom'
 
 class ManageBooks extends Component {
 
     constructor(props) {
         super(props);
-        this.handleShow = this.handleShow.bind(this)
-        this.handleClose = this.handleClose.bind(this)
-        this.handleImageChange = this.handleImageChange.bind(this);
-        this.handleContentChange = this.handleContentChange.bind(this);
-        this.storeBook = this.storeBook.bind(this);
+        this.getAllBooks = this.getAllBooks.bind(this);
         this.state = {
-            show: false,
-
-            bookImageUrl: null,
-            bookImageFile: null,
-
-            bookContentUrl: null,
-            bookContentFile: null
+            books: []
         }
     }
 
     componentDidMount() {
-
+        this.getAllBooks();
     }
     componentWillUnmount() {
 
     }
 
-    handleClose() {
-        this.setState({ show: false });
-    }
-    handleShow() {
-        this.setState({ show: true });
-    }
-
-    handleImageChange(e) {
-        const file = e.target.files[0];
-        if (file) {
-            const fileName = file.name;
-            this.setState({
-                bookImageUrl: URL.createObjectURL(file) || null, // Reset to null when new image selected
-                bookImageFile: file
-            });
-        }
-    }
-    // Update handleContentChange method to handle PDF files
-    handleContentChange(e) {
-        const file = e.target.files[0];
-        if (file) {
-            const fileName = file.name;
-            // Ensure the file type is PDF before setting the state
-            if (file.type === 'application/pdf') {
-                this.setState({
-                    bookContentUrl: URL.createObjectURL(file),
-                    bookContentFile: file
-                });
-            } else {
-                alert('Please select a PDF file.');
-            }
-        }
-    }
-
-
-    storeBook = async (event) => {
-        event.preventDefault();
-
-        const selectedImageFile = this.state.bookImageFile;
-        const selectedImageContent = this.state.bookContentFile
+    getAllBooks() {
         const apiLinks = [
-            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/book-image',
-            'http://localhost:3306/api/book-image',
-            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/book-content',
-            'http://localhost:3306/api/book-content'
-        ];
+            'https://suico-it3202-sqa-finalproject-backend.onrender.com/api/books/',
+            'http://localhost:3306/api/books/'
+        ]
 
-        if (selectedImageFile) {
-            const formImageData = new FormData();
-            formImageData.append('file', selectedImageFile);
-
-            try {
-                const response = await axios.post(apiLinks[0], formImageData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    },
+        axios.get(
+            apiLinks[0]
+        ).then(
+            (response) => {
+                // console.log(response.data.data);
+                this.setState({ books: response.data.data }, () => {
+                    console.log(this.state.books);
                 });
-                console.log('File uploaded successfully:', response.data);
-                console.log('Public URL:', response.data.imageUrl);
-
-                if (selectedImageContent) {
-                    const formContentData = new FormData();
-                    formContentData.append('file', selectedImageContent);
-
-                    try {
-                        const response = await axios.post(apiLinks[2], formContentData, {
-                            headers: {
-                                'Content-Type': 'multipart/form-data'
-                            },
-                        });
-                        console.log('File uploaded successfully:', response.data);
-                        console.log('Public URL:', response.data.pdfUrl);
-                    } catch (error) {
-                        console.error('Error uploading file:', error);
-                    }
-                }
-            } catch (error) {
-                console.error('Error uploading file:', error);
             }
-        }
-    };
+        ).catch(
+            (error) => {
+                console.log(error)
+            }
+        );
+    }
 
     render() {
         return (
@@ -131,48 +63,37 @@ class ManageBooks extends Component {
                             <FaSearch />
                         </Button>
                     </InputGroup>
-
-                    <Button variant="success" onClick={this.handleShow}>
-                        <FaBook /> Create New Book
-                    </Button>
-
-                    <Modal show={this.state.show} onHide={this.handleClose} size='lg'>
-                        <Modal.Header closeButton>
-                            <Modal.Title><FaBook /> Create New Book</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Form>
-                                <div style={{ display: 'inline-flex' }}>
-                                    <div id='leftDiv'>
-                                        {this.state.bookImageUrl ? (
-                                            <img
-                                                src={this.state.bookImageUrl}
-                                                alt="Book Cover"
-                                                height={300}
-                                                width={200}
-                                            />
-                                        ) : (
-                                            <img src="" alt="" height={300} width={200} />
-                                        )}
-                                    </div>
-                                    <div id='rightDiv' style={{ flex: '1', marginLeft: '20px', width: '469px' }}>
-                                        <Form.Group controlId="formFile" className="mb-3">
-                                            <Form.Control type="file" onChange={this.handleImageChange} />
-                                        </Form.Group>
-                                        <Form.Group controlId="formFile" className="mb-3">
-                                            <Form.Control type="file" onChange={this.handleContentChange} />
-                                        </Form.Group>
-                                        <Button variant="success" onClick={this.storeBook} type='submit'>Store Book</Button>
-                                    </div>
-                                </div>
-                            </Form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
-                                Close
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
+                    <CreateModalBook /><br />
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Book Image</th>
+                                <th>Book Title</th>
+                                <th>Book Author</th>
+                                <th>Book Genre</th>
+                                <th>Book Content</th>
+                                <th>Update</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.books.map((book) => (
+                                <tr key={book.id}>
+                                    <td><img src={book.image} height={75} width={75} alt="" /></td>
+                                    <td style={{ wordWrap: "break-word", wordBreak: "break-word", maxWidth: "150px" }}>{book.name}</td>
+                                    <td>{book.author}</td>
+                                    <td>{book.genre}</td>
+                                    <td>
+                                        <a href={book.content} target="_blank" rel="noopener noreferrer">
+                                            <Button variant="primary"><FaBookOpen /></Button>
+                                        </a>
+                                    </td>
+                                    <td><Button variant="warning"><FaSync /></Button></td>
+                                    <td><Button variant="danger"><FaTrash /></Button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
                 </div>
             </div>
         );
