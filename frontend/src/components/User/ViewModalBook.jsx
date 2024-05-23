13e5ce65d0371a97e3cf51c98e11949763a121bb
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-05-23 17:28:04 +0800

    Need to fix the Borrow Book disable button

--- frontend/src/components/User/ViewModalBook.jsx
+++ frontend/src/components/User/ViewModalBook.jsx
@@ -13,9 +13,12 @@ class ViewModalBook extends Component {
         super();
         this.handleShow = this.handleShow.bind(this)
         this.handleClose = this.handleClose.bind(this)
+        this.createBorrowRequest = this.createBorrowRequest.bind(this);
         this.state = {
             show: false,
             loading: false,
+
+            userId: localStorage.getItem("userId"),
         }
     }
 
@@ -33,6 +36,33 @@ class ViewModalBook extends Component {
         this.setState({ show: true });
     }
 
+    createBorrowRequest = (bookId) => {
+        console.log(this.state.userId + " ", bookId);
+
+        const data = {
+            user_id_fk: this.state.userId,
+            book_id_fk: bookId
+        }
+
+        const apiLink = [
+            `https://suico-it3202-sqa-finalproject-backend.onrender.com/api/borrow-books-request`,
+            `http://localhost:3306/api/borrow-books-request`
+        ];
+
+        axios.post(
+            apiLink[0], data
+        ).then(
+            (response) => {
+                console.log(response)
+                window.location.reload();
+            }
+        ).catch(
+            (error) => {
+                console.log(error)
+            }
+        );
+    }
+
     render() {
         const { show, handleClose, book } = this.props;
 
@@ -43,7 +73,26 @@ class ViewModalBook extends Component {
                         <Modal.Title><FaBook /> {book.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        
+                        <div style={{ display: 'inline-flex' }}>
+                            <div id='leftDiv'>
+                                <img
+                                    src={book.image}
+                                    alt="Book Cover"
+                                    height={300}
+                                    width={200}
+                                />
+                            </div>
+                            <div id='rightDiv' style={{ flex: '1', marginLeft: '20px', width: '469px' }}>
+                                <h2 style={{ marginBottom: "20px" }}>Title: {book.name}</h2>
+                                <h2 style={{ marginBottom: "20px" }}>Author: {book.author}</h2>
+                                <h2 style={{ marginBottom: "30px" }}>Genre: {book.genre}</h2>
+                                <div style={{ textAlign: "center" }}>
+                                    <Button 
+                                        variant="success" 
+                                        onClick={() => this.createBorrowRequest(book.id)}><h4><FaBook />  Borrow Book</h4></Button>
+                                </div>
+                            </div>
+                        </div>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
--- frontend/src/pages/Librarian/LibrarianBorrowBooks.jsx
+++ frontend/src/pages/Librarian/LibrarianBorrowBooks.jsx
@@ -1,16 +1,20 @@
 import { Component, useState } from 'react'
-import { Container, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown, Button, Form, InputGroup, Dropdown, Table } from 'react-bootstrap'
 import webName from '../../assets/website name.jpg'
 import ClockComponent from '../../components/ClockComponent'
 import LibrarianSidebar from '../../components/Librarian/LibrarianSidebar'
 import LibrarianNavbar from '../../components/Librarian/LibrarianNavbar'
+import { FaCheckSquare } from 'react-icons/fa' 
 
 class LibrarianBorrowBooks extends Component {
 
     constructor() {
         super();
         this.state = {
-
+            borrowBooksRequest: [],
+            searchInput: "",
+            selectBookGenreOption: "All",
+            filteredBooks: [],
         }
     }
 
@@ -21,12 +25,97 @@ class LibrarianBorrowBooks extends Component {
 
     }
 
+    getBorrowBooksRequests() {
+        const apiLink = [
+            `https://suico-it3202-sqa-finalproject-backend.onrender.com/api/borrow-books-request`,
+            `http://localhost:3306/api/borrow-books-request`
+        ];
+
+        axios.get(apiLink[0])
+            .then(response => {
+                console.log("API Response:", response.data.data);
+                this.setState({ borrowBooksRequest: response.data.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    searchBook = (event) => {
+        const searchInput = event.target.value;
+        this.setState({ searchInput }, () => {
+            const searchQuery = searchInput.toLowerCase();
+            if (searchQuery === "") {
+                this.setState({ filteredBooks: this.state.borrowBooksRequest });
+            } else {
+                const filteredBooks = this.state.borrowBooksRequest.filter(book =>
+                    book.name.toLowerCase().includes(searchQuery) ||
+                    book.author.toLowerCase().includes(searchQuery) ||
+                    book.genre.toLowerCase().includes(searchQuery)
+                );
+                this.setState({ filteredBooks });
+            }
+        });
+    }
+
+    handleOptionBookSelect = (option) => {
+        this.setState({ selectBookGenreOption: option }, () => {
+            this.getAllBooks(option);
+        });
+    };
+
     render() {
+        const { selectBookGenreOption } = this.state;
+
         return (
             <div>
                 <LibrarianNavbar />
                 <div>
                     <h1>Borrow Books Request Page</h1>
+                    <div style={{ padding: "1%", textAlign: "center" }}>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                placeholder="Enter Book Information"
+                                value={this.state.searchInput}
+                                onChange={this.searchBook}
+                            />
+                        </InputGroup>
+                        <div style={{ display: "inline-flex", gap: "20px" }}>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                                    {selectBookGenreOption}
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("All")}>All</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Historical Fiction")}>Historical Fiction</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Crime and Mystery")}>Crime and Mystery</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Horror")}>Horror</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Science Fiction")}>Science Fiction</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Education")}>Education</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Romance")}>Romance</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => this.handleOptionBookSelect("Fantasy")}>Fantasy</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                        <br /><br />
+                        <div style={{ textAlign: "center", height: "600px", overflowY: "auto" }}>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Book Image</th>
+                                        <th>Book Title</th>
+                                        <th>Book Author</th>
+                                        <th>Book Genre</th>
+                                        <th>View Request</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
