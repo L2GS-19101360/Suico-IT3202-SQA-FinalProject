e3eeb6457f321bbd39089323be26d6360185f1c5
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-05-26 10:14:53 +0800

    Update users.model.js

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -24,7 +24,7 @@ const User = function (user) {
 }
 
 User.viewAllReturnedBooks = function(result) {
-    dbConnection.query(`SELECT return_books_request.id, user.image as userImage, CONCAT(user.firstname, ' ', user.lastname) as userFullname, user.email as userEmail, books.image, books.name, books.author, books.genre, librarian.image as librarianImage, CONCAT(librarian.firstname, ' ', librarian.lastname) as librarianFullname, librarian.email as librarianEmail, return_books_request.returned_status, return_books_request.returned_updated FROM return_books_request JOIN  users AS user ON user.id = return_books_request.user_id_fk JOIN users AS librarian ON librarian.id = return_books_request.librarian_id_fk JOIN books ON books.id = return_books_request.book_id_fk WHERE return_books_request.returned_status = "Returned"`, function (err, res) {
+    dbConnection.query(`SELECT return_books_request.id, user.image AS userImage, user.firstname AS userFirstname, user.lastname AS userLastname, user.email AS userEmail, books.image, books.name, books.author, books.genre, librarian.image AS librarianImage, librarian.firstname AS librarianFirstname, librarian.lastname AS librarianLastname, librarian.email AS librarianEmail, return_books_request.returned_status, return_books_request.returned_updated FROM return_books_request JOIN users AS user ON user.id = return_books_request.user_id_fk JOIN users AS librarian ON librarian.id = return_books_request.librarian_id_fk JOIN books ON books.id = return_books_request.book_id_fk WHERE return_books_request.returned_status = 'Returned'`, function (err, res) {
         if (err) {
             console.log(err);
             result(null, err);
@@ -36,7 +36,7 @@ User.viewAllReturnedBooks = function(result) {
 }
 
 User.viewAllBorrowedBooks = function (result) {
-    dbConnection.query(`SELECT borrow_books_request.id, user.image as userImage, CONCAT(user.firstname, ' ', user.lastname) as userFullname, user.email as userEmail, books.image, books.name, books.author, books.genre, librarian.image as librarianImage, CONCAT(librarian.firstname, ' ', librarian.lastname) as librarianFullname, librarian.email as librarianEmail, borrow_books_request.borrowed_status, borrow_books_request.borrowed_updated FROM borrow_books_request JOIN  users AS user ON user.id = borrow_books_request.user_id_fk JOIN users AS librarian ON librarian.id = borrow_books_request.librarian_id_fk JOIN books ON books.id = borrow_books_request.book_id_fk WHERE borrow_books_request.borrowed_status = "Borrowed"`, function (err, res) {
+    dbConnection.query(`SELECT borrow_books_request.id, user.image AS userImage, user.firstname AS userFirstname, user.lastname AS userLastname, user.email AS userEmail, books.image, books.name, books.author, books.genre, librarian.image AS librarianImage, librarian.firstname AS librarianFirstname, librarian.lastname AS librarianLastname, librarian.email AS librarianEmail, borrow_books_request.borrowed_status, borrow_books_request.borrowed_updated FROM borrow_books_request JOIN users AS user ON user.id = borrow_books_request.user_id_fk JOIN users AS librarian ON librarian.id = borrow_books_request.librarian_id_fk JOIN books ON books.id = borrow_books_request.book_id_fk WHERE borrow_books_request.borrowed_status = 'Borrowed'`, function (err, res) {
         if (err) {
             console.log(err);
             result(null, err);
